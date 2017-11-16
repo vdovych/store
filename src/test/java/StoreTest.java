f8d149a37dfc7e716018241bc7bdfe792b475027
vdovych <vdovych@ucu.edu.ua>
2017-11-16 19:36:45 +0200

    New test

--- src/test/java/StoreTest.java
+++ src/test/java/StoreTest.java
@@ -1,9 +1,11 @@
 import org.junit.Test;
 import store.*;
 
+import java.util.LinkedList;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StoreTest {
     @Test
@@ -70,7 +72,37 @@ public class StoreTest {
         assertEquals(3, store.search(spec).size());
         assertEquals(0, store.search(null).size());
     }
-
+    @Test
+    public void TestPaymentDelivery(){
+        PaymentStrategy Cash = new CashPayment();
+        PaymentStrategy Privat = new Privat24Paymen();
+        assertTrue(Cash.pay(1));
+        assertTrue(Privat.pay(1));
+        DelivetyStrategy NP = new DeliveryNovaPoshta();
+        DelivetyStrategy DHL = new DeliveryDHL();
+        assertTrue(NP.deliver(new LinkedList<ComputerGame>()));
+        assertTrue(DHL.deliver(new LinkedList<ComputerGame>()));
+    }
+    @Test
+    public void TestCart(){
+        Cart cart = new Cart();
+        PaymentStrategy ps = new CashPayment();
+        DelivetyStrategy ds = new DeliveryNovaPoshta();
+        cart.setPaymentStrategy(ps);
+        cart.setDelivetyStrategy(ds);
+        assertEquals(ps,cart.getPaymentStrategy());
+        assertEquals(ds,cart.getDelivetyStrategy());
+        for (int i = 0; i < 3; i++)
+           cart.add(new ComputerGame("TestGame","16/11/2017",new GameSpec(Typ.MMORPG, Developer.EA),10));
+        assertEquals(30,cart.computeTotalPrice(),0.01);
+        assertTrue(cart.ship());
+    }
+    @Test
+    public void TestDecorators(){
+        CartDecorator bonus = new BonusDecorator(new Cart());
+        CartDecorator discount = new DiscountDecorator(new Cart());
+        assertTrue(true);
+    }
 
 
 }
